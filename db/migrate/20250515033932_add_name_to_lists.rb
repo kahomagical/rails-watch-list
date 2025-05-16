23fefb5ef0e05bbaec2b287ca82d6d651130b860
Kaho Maeda <kaho@tutamail.com>
2025-05-16 12:53:11 +0900

    fix migrations

--- db/migrate/20250515033932_add_name_to_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToLists < ActiveRecord::Migration[7.1]
-  def change
-    add_column :lists, :name, :string
-  end
-end
--- db/migrate/20250515034309_add_columns_to_bookmarks.rb
+++ db/migrate/20250515034309_add_columns_to_bookmarks.rb
@@ -1,6 +1,5 @@
 class AddColumnsToBookmarks < ActiveRecord::Migration[7.1]
   def change
-    add_column :bookmarks, :comment, :string
     add_reference :bookmarks, :movie, foreign_key: true
     add_reference :bookmarks, :list, foreign_key: true
   end
