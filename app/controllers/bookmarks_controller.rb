3fbdbe71e48080e52cf9dab7276441cae5d7b786
Kaho Maeda <kaho@tutamail.com>
2025-05-19 07:55:02 +0900

    working on show

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,8 @@
 class BookmarksController < ApplicationController
+  def show
+    @bookmarks = Bookmark.all
+  end
+
   def new
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
